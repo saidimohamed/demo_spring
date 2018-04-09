8f8c0e663d4ba3ec78136a5bda8a1b0ab299b878
yaskomega <yh@yassinhassan.com>
2018-04-09 16:36:11 +0200

    Changing uri to fit the tomcat server architecture. Adding dependencies and modifying Application class to generate the war file.

--- src/main/java/com/soprasteria/demo_spring/DemoSpringApplication.java
+++ src/main/java/com/soprasteria/demo_spring/DemoSpringApplication.java
@@ -1,10 +1,18 @@
 package com.soprasteria.demo_spring;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoSpringApplication {
+public class DemoSpringApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DemoSpringApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoSpringApplication.class, args);
