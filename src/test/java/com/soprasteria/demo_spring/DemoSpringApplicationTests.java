b733095ecaab886c331c14ff7e77e24f1e9da3db
yaskomega <yh@yassinhassan.com>
2018-04-25 16:31:14 +0200

    Adding tests

--- src/test/java/com/soprasteria/demo_spring/DemoSpringApplicationTests.java
+++ src/test/java/com/soprasteria/demo_spring/DemoSpringApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +12,24 @@ public class DemoSpringApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void test1() {
+        int i = 0;
+        Assert.assertTrue(i == 0);
+    }
+
+    @Test
+    public void test2() {
+        int i = 0;
+        Assert.assertTrue(i != 1);
+    }
+
+    @Test
+    public void test3() {
+        int i = 0;
+        Assert.assertEquals("",0, i, 0);
     }
 
 }
